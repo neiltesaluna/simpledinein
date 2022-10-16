ff51941f1f7a08d468bb78320e86fff98c6a60d5
Neil Tesaluna <neil.tesaluna@gmail.com>
2022-10-17 03:42:52 +1100

    renamed custom filter markdown function

--- django/simpledinein/simpledinein/recipe/templatetags/markdown.py
+++ django/simpledinein/simpledinein/recipe/templatetags/markdown.py
@@ -1,10 +1,10 @@
 from django import template
 from django.template.defaultfilters import stringfilter
-import markdown as md
+import markdown
 
 register = template.Library()
 
 @register.filter(name='markdown')
 @stringfilter
-def markdown(value):
-    return md.markdown(value, extensions=['md_in_html'])
+def markdown_filter(value):
+    return markdown.markdown(value, extensions=['md_in_html'])
