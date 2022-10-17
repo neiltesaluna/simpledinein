7d38a9887a8f16a567aae5f5b3a00276ca768e22
Neil Tesaluna <neil.tesaluna@gmail.com>
2022-10-17 17:21:06 +1100

    added class based views for recipes

--- django/simpledinein/simpledinein/recipe/urls.py
+++ django/simpledinein/simpledinein/recipe/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import RecipeListView, RecipeDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='all_recipes'),
+    path('', RecipeListView.as_view(), name='all_recipes'),
+    path('<int:pk>', RecipeDetailView.as_view(), name='recipe_detail')
 ]
--- django/simpledinein/simpledinein/recipe/views.py
+++ django/simpledinein/simpledinein/recipe/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Recipe
 
 def home(request):
   return render(request, 'recipe/home.html', context = {'recipes': Recipe.objects.all()})
+
+class RecipeListView(ListView):
+  model = Recipe
+  template_name = 'recipe/home.html'
+  context_object_name = 'recipes'
+  ordering = ['title']
+
+class RecipeDetailView(DetailView):
+  model = Recipe
