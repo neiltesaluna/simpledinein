7231c45cf7afdffd1bbcc5b6c2c12253ac708967
Neil Tesaluna <neil.tesaluna@gmail.com>
2022-10-17 17:34:20 +1100

    removed function based views

--- django/simpledinein/simpledinein/recipe/views.py
+++ django/simpledinein/simpledinein/recipe/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
 
-def home(request):
-  return render(request, 'recipe/home.html', context = {'recipes': Recipe.objects.all()})
-
 class RecipeListView(ListView):
   model = Recipe
   template_name = 'recipe/home.html'
