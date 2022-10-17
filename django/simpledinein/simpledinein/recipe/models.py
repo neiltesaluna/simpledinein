89fe79f9067108c547d92c1e0bf6b1ea325e0a64
Neil Tesaluna <neil.tesaluna@gmail.com>
2022-10-18 01:12:28 +1100

    added css styling, navbar and static image

--- django/simpledinein/simpledinein/recipe/models.py
+++ django/simpledinein/simpledinein/recipe/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Recipe(models.Model):
-  title = models.CharField(max_length=1000)
+  title = models.CharField(max_length=150)
+  summary = models.TextField(max_length=1000)
   content = models.TextField()
 
   def __str__(self):
