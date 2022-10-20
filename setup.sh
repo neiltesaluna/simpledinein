985808b4ab952b87d757ce2ccf003f1b223d5b28
Neil Tesaluna <neil.tesaluna@gmail.com>
2022-10-21 00:25:50 +1100

    update admin import from env variables in bash script

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata recipes.json
-python manage.py --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('${DJANGO_SUPERUSER_USERNAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')" | python manage.py shell
+python manage.py runserver 0.0.0.0:8000
