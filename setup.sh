59b1bf0977d6db9343c97c5a9d5e85b95668f4d6
Neil Tesaluna <neil.tesaluna@gmail.com>
2022-10-21 09:34:19 +1100

    changed dockerfile

--- setup.sh
+++ setup.sh
@@ -3,4 +3,3 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata recipes.json
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('${DJANGO_SUPERUSER_USERNAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')" | python manage.py shell
-python manage.py runserver 0.0.0.0:8000
