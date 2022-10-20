82796dfb8101aaf4e63905305a857e1f021f218d
Neil Tesaluna <neil.tesaluna@gmail.com>
2022-10-21 00:07:45 +1100

    added admin login env variables

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata recipes.json
+python manage.py --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
