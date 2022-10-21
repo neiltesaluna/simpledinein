f4d0f40e4b0946852f6f0e7f248eaf649d04bb36
Neil Tesaluna <neil.tesaluna@gmail.com>
2022-10-22 08:35:24 +1100

    changing superuser auto setup

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata recipes.json
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('${DJANGO_SUPERUSER_USERNAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')" | python manage.py shell
+python manage.py createsuperuser --noinput
