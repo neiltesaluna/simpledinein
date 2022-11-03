e65cee1b561665fd4795686ae1b1ec2ef6e6a885
Neil Tesaluna <neil.tesaluna@gmail.com>
2022-11-03 13:17:15 +1100

    added an api link on homepage navbar

--- simpledinein/api/urls.py
+++ simpledinein/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.getData),
+  path('', views.getData, name='api_link'),
   path('<int:pk>', views.recipeDetail),
 ]
