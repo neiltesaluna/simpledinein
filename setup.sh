281377a0d7aea826cbaccc7a9e2a46d4c9fe1264
Neil Tesaluna <neil.tesaluna@gmail.com>
2022-11-11 13:33:09 +1100

    updated required packages

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata recipes.json
 python manage.py createsuperuser --noinput
-python manage.py runserver 0.0.0.0:8000
+gunicorn simpledinein.wsgi --bind 0.0.0.0:8000 --workers=$(nproc)
