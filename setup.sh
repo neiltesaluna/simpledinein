b5f8639039a155675e717d211c0fbadcccf40901
Neil Tesaluna <neil.tesaluna@gmail.com>
2022-12-20 20:01:37 +1100

    deleted preload recipes

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@
 python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata recipes.json
 python manage.py createsuperuser --noinput
 gunicorn simpledinein.wsgi --bind 0.0.0.0:8000 --workers=$(nproc)
